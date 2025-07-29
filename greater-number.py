b5824133232bd8611dd1b6ddc16d6fdc46d11543
Shwetk88 <khamkarshweta08@gmail.com>
2025-07-29 15:07:24 +0530

    Add files via upload

--- greater-number.py
+++ greater-number.py
@@ -1,9 +1,14 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+# greater-number.py
+num1 = 10
+num2 = 20
 
 if num1 > num2:
-    print(f"The greater number is: {num1}")
+    print(f"{num1} is greater than {num2}")
 elif num2 > num1:
-    print(f"The greater number is: {num2}")
+    print(f"{num2} is greater than {num1}")
 else:
-    print("Both numbers are equal.")
+    print("Both numbers are equal")
+
+
+
+
